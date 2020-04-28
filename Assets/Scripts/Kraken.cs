8a786e18ffb19623de9197a27f1510a15a1ee7d8
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2020-04-28 21:27:00 +0200

    Spawning 3 tentacles and destroy them after a certain time
    Stuck on the AI controlling one though....

--- Assets/Scripts/Kraken.cs
+++ Assets/Scripts/Kraken.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class Kraken : MonoBehaviour
 {
+    public int amount;
     public GameObject theKraken;
     public Vector3 center;
-    public Vector3 size;
-
+    public Vector3 size = new Vector3(1000, 0, 1000);
+    public int TimeKrakenVisible;
     private void Update()
     {
-        SpawnKraken();
     }
-    private void OnCollisionEnter(Collision col)
+    private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Player")
         {
-            SpawnKraken();
-            Destroy(this.gameObject);
+            StartCoroutine("SpawnKraken");
+            this.gameObject.GetComponent<Collider>().enabled = false;
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
     }
 
-    public void SpawnKraken()
+    IEnumerator SpawnKraken()
     {
-        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
 
-        Instantiate(theKraken, pos, Quaternion.identity);
+            Instantiate(theKraken, pos, Quaternion.identity);
+
+        }
+        yield return new WaitForSeconds(TimeKrakenVisible);
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Kraken");
+        for (int i = 0; i < objects.Length; i++)
+        {
+            Destroy(objects[i]);
+        }
+        Destroy(this.gameObject);
     }
 }
