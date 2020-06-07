722c4b4e2461db2dd692a6507f9340415ad88559
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2020-06-07 11:36:49 +0200

    stuff

--- Assets/Scripts/AIs/GradusAI.cs
+++ Assets/Scripts/AIs/GradusAI.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 
 public class GradusAI : Ship
 {
+    [SerializeField]private bool lowHealth;
     public override IEnumerator RunAI(object caller)
     {
         if (!(caller is CompetitionManager)) yield break;
         while (!dying || !CompetitionManager.current.gameOver || !CompetitionManager.current.gameStarted)
         {
-            yield return MoveForward(200);
+            if(health <= 50)
+            {
+                lowHealth = true;
+            }
+            if(lowHealth == true)
+            {
+                
+            }
+            yield return MoveForward(10);
         }
+    }
+
+    private IEnumerator MoveTowards(Vector3 posTarget)
+    {
+        yield return null;
     }
 }
