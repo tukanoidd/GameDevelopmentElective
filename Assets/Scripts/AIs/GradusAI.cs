27222aff8f91e4218c6949d69aff876424d039af
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2020-06-09 09:15:10 +0200

    new ship

--- Assets/Scripts/AIs/GradusAI.cs
+++ Assets/Scripts/AIs/GradusAI.cs
@@ -1,30 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using BaseScripts;
 using UnityEngine;
 
 public class GradusAI : Ship
 {
-    [SerializeField]private bool lowHealth;
+    private bool lowHealth => health <= 50;
+    private bool hasHealing => powerup == PowerUpType.Healing;
+
+    private bool canHeal => lowHealth && hasHealing;
+
+    private Ship Enemyship => visibleShips.Where(ship => Vector3.Distance(position, ship.position) <= 200 && !ship.dying)
+        .OrderBy(ship => ship.health).FirstOrDefault();
     public override IEnumerator RunAI(object caller)
     {
         if (!(caller is CompetitionManager)) yield break;
         while (!dying || !CompetitionManager.current.gameOver || !CompetitionManager.current.gameStarted)
         {
-            if(health <= 50)
-            {
-                lowHealth = true;
-            }
+            
             if(lowHealth == true)
             {
-                
+                Debug.Log("Need Healing");
+
+                IEnumerable<PowerUp> healingPowerUps =
+                          visiblePowerUps.Where(powerUp => powerUp.powerUpType == PowerUpType.Healing);
+
+                if (healingPowerUps.Any())
+                {
+                    PowerUp nearestHealingPowerUp = healingPowerUps.OrderBy(powerUp =>
+                        Vector3.Distance(powerUp.transform.position, position)).First();
+                    Debug.Log("Let's do some healing");
+                    yield return MoveTowards(nearestHealingPowerUp.transform.position);
+                    yield break;
+                }
             }
             yield return MoveForward(10);
         }
     }
 
+    private IEnumerator RotateTowards(Vector2 direction)
+    {
+        float angle = Vector2.Angle(direction, transform.forward);
+        yield return Rotate(angle, Direction.Left);
+    }
     private IEnumerator MoveTowards(Vector3 posTarget)
     {
-        yield return null;
+        Vector2 AIMapPos = new Vector2(position.x, position.z);
+        Vector2 targetMapPos = new Vector2(posTarget.x, posTarget.z);
+
+        yield return visionSphere.MoveToDirection(VisionSphere.VisionPosition.Front);
+        yield return RotateTowards(targetMapPos - AIMapPos);
+        yield return MoveForward(Vector2.Distance(targetMapPos, AIMapPos));
     }
 }
