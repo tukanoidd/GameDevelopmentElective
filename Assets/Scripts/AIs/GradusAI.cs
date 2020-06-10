ff93be1db182428756499931a116a3badaec357c
Gradus Smid <33928046+GradusSmid@users.noreply.github.com>
2020-06-10 13:21:35 +0200

    Some small changes Found some errors
    Sorry boys this should be it

--- Assets/Scripts/AIs/GradusAI.cs
+++ Assets/Scripts/AIs/GradusAI.cs
@@ -11,7 +11,7 @@ public class GradusAI : Ship
     private bool hasHealing => powerup == PowerUpType.Healing;
 
     private bool canHeal => lowHealth && hasHealing;
-    private float SeaSize = 450;
+    private float SeaSize = 350;
     private Ship Enemyship => visibleShips.Where(ship => Vector3.Distance(position, ship.position) <= 200 && !ship.dying)
         .OrderBy(ship => ship.health).FirstOrDefault();
     public override IEnumerator RunAI(object caller)
@@ -21,7 +21,20 @@ public class GradusAI : Ship
         {
             if (!visibleShips.Any() && !visiblePowerUps.Any())
             {
-                yield return MoveVision();
+                VisionSphere.VisionPosition oldPos = visionSphere.position;
+                VisionSphere.VisionPosition newPos = oldPos == VisionSphere.VisionPosition.Back
+                    ? VisionSphere.VisionPosition.Left
+                    : (oldPos == VisionSphere.VisionPosition.Left
+                        ? VisionSphere.VisionPosition.Front
+                        : (oldPos == VisionSphere.VisionPosition.Front
+                            ? VisionSphere.VisionPosition.Right
+                            : VisionSphere.VisionPosition.Back));
+
+                yield return visionSphere.MoveToDirection(newPos);
+                Shoot(VisionSphere.VisionPosition.Front);
+                Shoot(VisionSphere.VisionPosition.Left);
+                Shoot(VisionSphere.VisionPosition.Right);
+                yield return GradusMoveForward(200);
             }
             if (lowHealth)
             {
@@ -45,62 +58,63 @@ public class GradusAI : Ship
                 }
             }
 
-            if(visibleShips.Any() && !visiblePowerUps.Any())
+            if (visibleShips.Any() && !visiblePowerUps.Any())
             {
-                yield return Shoot();
+                yield return ShootGradus();
             }
 
-            if(powerup == PowerUpType.Kraken)
+            if (powerup == PowerUpType.Kraken)
             {
                 StartCoroutine(UseKrakenPowerUp());
             }
-            
-            if(visibleShips.Any() && health <=50 && powerup == PowerUpType.Speeder)
+
+            if (visibleShips.Any() && health <= 50 && powerup == PowerUpType.Speeder)
             {
                 StartCoroutine(UseSpeederPowerUp());
                 yield return Hide();
             }
-            if(visiblePowerUps.Any() && powerup != PowerUpType.None)
-            {
-                if(powerup == PowerUpType.Kraken)
-                {
-                    StartCoroutine(UseKrakenPowerUp());
-                }
-                if (powerup == PowerUpType.Speeder)
-                {
-                    StartCoroutine(UseSpeederPowerUp());
-                }
-                if (powerup == PowerUpType.FireBoat)
-                {
-                    StartCoroutine(UseFireBoatPowerUp());
-                }
-                if (powerup == PowerUpType.Healing)
-                {
-                    StartCoroutine(UseHealingPowerUp());
-                }
 
-                PowerUp powerNear = visiblePowerUps
-                        .OrderBy(powerUp => Vector3.Distance(powerUp.transform.position, position))
-                        .First();
-                MoveTowards(powerNear.transform.position);
+            if(visiblePowerUps.Any() && !visibleShips.Any())
+            {
+                yield return CloseToPowerUps();
+            }
+            if(visiblePowerUps.Any() && visibleShips.Any())
+            {
+                yield return CloseToPowerUps();
+                yield return ShootGradus();
             }
         }
     }
     private IEnumerator GradusMoveForward(float distance)
     {
         yield return MoveForward(distance);
-        if(Mathf.Abs(position.x) > SeaSize || Mathf.Abs(position.z) > SeaSize)
+        if (Mathf.Abs(position.x) > SeaSize || Mathf.Abs(position.z) > SeaSize)
         {
             Vector2 rotationDirection = new Vector2(-position.x, -position.z);
             yield return RotateTowards(rotationDirection);
         }
-        
+
+    }
+    private IEnumerator CloseToPowerUps()
+    {
+        UpdateVisiblePowerUps(visiblePowerUps);
+        if (visiblePowerUps.Any())
+        {
+            StartCoroutine(UseHealingPowerUp());
+            StartCoroutine(UseKrakenPowerUp());
+            StartCoroutine(UseSpeederPowerUp());
+            StartCoroutine(UseFireBoatPowerUp());
+            PowerUp nearbyPowerUp = visiblePowerUps
+                .OrderBy(powerUp => Vector3.Distance(powerUp.transform.position, position))
+                .First();
+            yield return MoveTowards(nearbyPowerUp.transform.position);
+        }
     }
     private IEnumerator RotateTowards(Vector2 direction)
     {
         Vector3 forward = transform.forward;
         float angle = Vector2.SignedAngle(direction, new Vector2(forward.x, forward.z));
-        if(Math.Abs(angle) > 0.10f)
+        if (Math.Abs(angle) > 0.10f)
             yield return Rotate(Math.Abs(angle), angle < 0 ? Direction.Left : Direction.Right);
     }
     private IEnumerator MoveTowards(Vector3 posTarget)
@@ -110,7 +124,7 @@ public class GradusAI : Ship
 
         yield return visionSphere.MoveToDirection(VisionSphere.VisionPosition.Front);
         yield return RotateTowards(targetMapPos - AIMapPos);
-        yield return MoveForward(Vector2.Distance(targetMapPos, AIMapPos));
+        yield return GradusMoveForward(Vector2.Distance(targetMapPos, AIMapPos));
     }
 
     private IEnumerator Hide()
@@ -118,7 +132,7 @@ public class GradusAI : Ship
         yield return Rotate(20, Direction.Right);
         yield return GradusMoveForward(100);
     }
-    private IEnumerator Shoot()
+    private IEnumerator ShootGradus()
     {
         Ship closestShip = visibleShips
                    .OrderBy(ship => Vector3.Distance(ship.transform.position, position))
@@ -128,40 +142,21 @@ public class GradusAI : Ship
         {
             StartCoroutine(UseFireBoatPowerUp());
         }
-        if (visionSphere.position == VisionSphere.VisionPosition.Left)
+        if (VisionSphere.VisionPosition.Left == visionSphere.position)
         {
             Shoot(VisionSphere.VisionPosition.Left);
-            yield break;
+            Debug.Log("Shoot left");
         }
-        if (visionSphere.position == VisionSphere.VisionPosition.Right)
+        if (VisionSphere.VisionPosition.Right == visionSphere.position)
         {
             Shoot(VisionSphere.VisionPosition.Right);
-            yield break;
+            Debug.Log("Shoot right");
         }
-        if (visionSphere.position == VisionSphere.VisionPosition.Back)
-        {
-            Shoot(VisionSphere.VisionPosition.Back);
-            yield break;
-        }
-        if (visionSphere.position == VisionSphere.VisionPosition.Front)
+        if (VisionSphere.VisionPosition.Front == visionSphere.position)
         {
             Shoot(VisionSphere.VisionPosition.Front);
-            yield break;
+            Debug.Log("Shoot front");
         }
-    }
-    private IEnumerator MoveVision()
-    {
-        VisionSphere.VisionPosition oldPos = visionSphere.position;
-        VisionSphere.VisionPosition newPos = oldPos == VisionSphere.VisionPosition.Back
-            ? VisionSphere.VisionPosition.Left
-            : (oldPos == VisionSphere.VisionPosition.Left
-                ? VisionSphere.VisionPosition.Front
-                : (oldPos == VisionSphere.VisionPosition.Front
-                    ? VisionSphere.VisionPosition.Right
-                    : VisionSphere.VisionPosition.Back));
-
-        yield return visionSphere.MoveToDirection(newPos);
-
-        yield return GradusMoveForward(200);
+        yield return GradusMoveForward(10);
     }
 }
